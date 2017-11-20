47ae29338d4e1a6466335d7a7e06aca68c4548bc
eleves <eleves@mac404inf-05.local>
2017-11-20 13:28:15 -0500

    All controllers

--- evaluation-plus/evaluation/EvaluationViewController.swift
+++ evaluation-plus/evaluation/EvaluationViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class EvaluationViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.tableFooterView = UIView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
